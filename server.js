6e08e9b28a47e0ebd76147bfabec94ec22b15b30
unknown <jdh9797@knu.ac.kr>
2021-08-04 13:44:19 +0900

    fix

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const request_ip=require('request-ip');
 
 const server=express();
 server.use(bodyparser.json());
+
 const users=[
     {
-        id:"ehgus",
-        name:"abs"
-    },
-    {
-        id:"ehgus2",
-        name:"abs2"
+        id:"dummy",
+        device_name: 'audio',
+        api: 'http://192.168.56.1:4000/music_on'
     }
 ];
 server.get("/list",(resquest,respond)=>{
@@ -32,19 +30,27 @@ server.get("/list/:id",(request,respond)=>{
 server.post("/register",(request,respond)=>{
     const device_ip=request_ip.getClientIp(request);
     const ip=device_ip.split(':');
+    console.log
+    console.log(request.body);
     
     const device_info=request.body.api='http://'+ip[3]+':'+request.body.port+'/'+request.body.function;
 
     delete request.body.port;
     delete request.body.function;
     request.body.api=device_info;
-    users.push(request.body);
+    var found=users.find((item,idx)=>{
+        return item.api==device_info;
+    });
+    if(!found){
+        users.push(request.body);
+    }
     console.log(request.body);
     
     respond.json(users);
 });
 
 server.get("/device/:id/:device_name",(req,res)=>{
+    console.log(req.params.id,req.params.device_name);
     let foundIndex=users.findIndex(u=>(u.id===req.params.id && u.device_name===req.params.device_name));
     if(!foundIndex){
         res.respond(404).json({errorMessage:"User was not found"});
@@ -54,9 +60,8 @@ server.get("/device/:id/:device_name",(req,res)=>{
 });
 
 server.get("/testIp",(req,res)=>{
-    console.log("client-IP: "+request_ip.getClientIp(req));
-    res.send('test');
-});
+    cli_Ip=request_ip.getClientIp(req);
+})
 
 // server.put("/update/:id",(req,res)=>{
 //     let foundItem=users.findIndex(u=>u.id===req.params.id);
